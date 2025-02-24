108bd6ec0d1ceb560a93cf64c96be05a63c2ab5b
Davidmitseruk <davidmitseruk@gmail.com>
2025-02-24 15:44:33 +0100

    ok

---
+++ http/script.js
@@ -0,0 +1,14 @@
+// const button = document.getElementById("getCatFact");
+/// const p = document.getElementById("catFact")
+
+// button.addEventListener('click', () => {
+//     fetch('https://catfact.ninja/fact')
+//         .then(response => response.json())
+//         .then(data => {
+//             p.textContent = data.fact;
+//         })
+//         .catch(error => {
+//             p.textContent = 'error in the matrix 😿'
+//             console.log("error: ", error)
+//         })
+// })
